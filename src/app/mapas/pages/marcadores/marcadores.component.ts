9120effafde22e7bfc2d9291156cabaa01ae2f02
Marcos <marcos.ledesma.picazo@cieep.com>
2021-06-19 14:44:49 +0200

    Marcadores en el mapa

--- src/app/mapas/pages/marcadores/marcadores.component.ts
+++ src/app/mapas/pages/marcadores/marcadores.component.ts
@@ -1,16 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { ElementRef, AfterViewInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
 
 @Component({
   selector: 'app-marcadores',
   templateUrl: './marcadores.component.html',
   styles: [
+    `
+      .mapa-container {
+          width: 100%;
+          height: 100%;
+        }
+    `
   ]
 })
-export class MarcadoresComponent implements OnInit {
+export class MarcadoresComponent implements AfterViewInit {
+
+  @ViewChild('mapa') divMapa!: ElementRef;
+  mapa!: mapboxgl.Map;
+  zoomLevel: number = 15;
+  center: [number, number] = [ -0.36042507119481876, 39.488717334091724 ];
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
+    this.mapa = new mapboxgl.Map({
+      container: this.divMapa.nativeElement ,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: this.center, // Longitud y latitud
+      zoom: this.zoomLevel
+    });
+
+    const markerHtml: HTMLElement = document.createElement('div');
+    markerHtml.innerHTML = "Hola";
+
+    const marker = new mapboxgl.Marker({
+      element: markerHtml
+    })
+      .setLngLat( this.center )
+      .addTo( this.mapa );
+
   }
 
 }

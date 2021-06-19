7d1407c92cbc69c239b98329ddeed8f60b80e96c
Marcos <marcos.ledesma.picazo@cieep.com>
2021-06-19 10:45:52 +0200

    Mapa en pantalla completa

--- src/app/mapas/pages/full-screen/full-screen.component.ts
+++ src/app/mapas/pages/full-screen/full-screen.component.ts
@@ -1,9 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-full-screen',
   templateUrl: './full-screen.component.html',
   styles: [
+    `
+      #mapa {
+        width: 100%;
+        height: 100%;
+      }
+    `
   ]
 })
 export class FullScreenComponent implements OnInit {
@@ -11,6 +19,14 @@ export class FullScreenComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+
+    (mapboxgl as any).accessToken = environment.mapboxToken;
+
+    var map = new mapboxgl.Map({
+    container: 'mapa',
+    style: 'mapbox://styles/mapbox/streets-v11'
+    });
+
   }
 
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  mapboxToken: 'pk.eyJ1IjoibWFyY29zZGFtIiwiYSI6ImNrcTBxZTQ0ajAxZHQycnBmNDQ2YTBwZzEifQ.8S67KjnbFG5PXGxYQdY2XQ'
 };
